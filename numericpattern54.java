4726cf927063d37d242b9573b4836c39cc00172d
ArkaPrava07 <97127825+ArkaPrava07@users.noreply.github.com>
2022-01-15 17:07:51 +0530

    Added Pattern Problems in Java

---
+++ numericpattern54.java
@@ -0,0 +1,23 @@
+/**
+ * Pattern54
+    1 
+    0 1 
+    1 0 1 
+    0 1 0 1 
+    1 0 1 0 1 
+ */
+class numericpattern54 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i+j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
